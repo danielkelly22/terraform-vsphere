b5943ba849b9fe349c9a76138fdecd3dedf2c522
Stephen Zuk <Justbkuz@users.noreply.github.com>
2020-07-30 22:47:04 -0400

    readjust vars

--- Linux_Test_Deployment/vars.tf
+++ Linux_Test_Deployment/vars.tf
@@ -1,20 +1,14 @@
 variable "vsphere_server" {}
 variable "vsphere_user" {}
-variable "local_admin_user" {}
-variable "local_admin_password" {}
+variable "vsphere_admin_password" {}
 variable "vsphere_datacenter" {}
 variable "vsphere_linux_vm_folder" {}
-variable "vsphere_compute_cluster" {}
-variable "vsphere_datastore_cluster" {}
+variable "vsphere_linux_compute_cluster" {}
+variable "vsphere_linux_datastore_cluster" {}
 variable "vsphere_app_dev_network" {}
 variable "vsphere_linux_machine_template" {}
-variable "vsphere_machine_template" {}
-variable "vm_web_dev_ip_address" {}
-variable "vm_db_dev_ip_address" {}
+variable "vsphere_linux_machine_template" {}
 variable "vm_app_dev_ip_address" {}
-variable "vm_web_prod_ip_address" {}
-variable "vm_db_prod_ip_address" {}
-variable "vm_app_prod_ip_address" {}
 variable "domain_name" {}
 
 variable "vsphere_admin_password" {
