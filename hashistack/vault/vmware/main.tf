4dd8a2afa0c5011707fc33422e4b2382994ba49c
danielkelly22 <daniel.kelly22@gmail.com>
2020-11-19 15:10:26 -0500

    Update main.tf
    updating naming format

--- hashistack/vault/vmware/main.tf
+++ hashistack/vault/vmware/main.tf
@@ -122,7 +122,7 @@ resource "vsphere_virtual_machine" "transit" {
 
 resource "vsphere_virtual_machine" "storage" {
     for_each                    = var.storage_zones
-    name                        = "${var.info.name}vaultsto${format("%02d",each.key)}"
+    name                        = "${var.info.name}vaultsto${each.key}"
     resource_pool_id            = data.vsphere_compute_cluster.cluster.resource_pool_id
     datastore_id                = data.vsphere_datastore.datastore.id
     guest_id                    = data.vsphere_virtual_machine.template.guest_id
@@ -183,7 +183,7 @@ resource "null_resource" "install_storage" {
 
 resource "vsphere_virtual_machine" "vault" {
     for_each                    = var.vault_zones
-    name                        = "${var.info.name}vault${format("%02d",each.key)}""
+    name                        = "${var.info.name}vault${each.key}"
     resource_pool_id            = data.vsphere_compute_cluster.cluster.resource_pool_id
     datastore_id                = data.vsphere_datastore.datastore.id
     guest_id                    = data.vsphere_virtual_machine.template.guest_id
