e76d508a080cb0bb5fe1960eabc3423cd1dd90e5
danielkelly22 <daniel.kelly22@gmail.com>
2020-11-23 16:09:27 -0500

    Update outputs.tf
    removing comments

--- hashistack/vault/vmware/outputs.tf
+++ hashistack/vault/vmware/outputs.tf
@@ -12,9 +12,9 @@
 
 output "outputs" {
     value               = {
-        //storage_ssh         = local.storage_ssh
-        //vault_ssh           = local.vault_ssh
-        //vault_http          = local.vault_http
+        storage_ssh         = local.storage_ssh
+        vault_ssh           = local.vault_ssh
+        vault_http          = local.vault_http
         transit_ssh         = "ssh -o stricthostkeychecking=no ${ var.image.username }@${ vsphere_virtual_machine.transit.default_ip_address } -y"
         transit_root_token  = data.external.transit.result.root_token
         transit_unseal_key  = data.external.transit.result.unseal_key
