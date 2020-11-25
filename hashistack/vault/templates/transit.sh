6a5d6c7b0ad03f902e697908e4da469c0915cd39
danielkelly22 <daniel.kelly22@gmail.com>
2020-11-25 12:10:02 -0500

    Update transit.sh
    testing

--- hashistack/vault/templates/transit.sh
+++ hashistack/vault/templates/transit.sh
@@ -13,15 +13,14 @@
 
 set -e 
 
-path=$(which jq)
-if [[ -z "$path" ]] ; then
-    path="./jq" 
-    if [[ ! -f "$path" ]] ; then
-        curl https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 > $path 
-        chmod 0755 $path 
-    fi
-fi
-echo $path 
+#path=$(which jq)
+#if [[ -z "$path" ]] ; then
+ #   path="./jq" 
+ #   if [[ ! -f "$path" ]] ; then
+ #       curl https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 > $path 
+ #       chmod 0755 $path 
+#    fi
+#fi
 
 #eval "$(${path} -r '@sh "export EXECUTION_PATH=\(.execution_path) USERNAME=\(.username) PUBLIC_IP=\(.public_ip) PRIVATE_KEY=\(.private_key)"')" > /tmp/tf_output/output.txt 2>&1
 
