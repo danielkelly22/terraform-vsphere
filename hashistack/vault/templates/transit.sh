8d021c671fc3cc373a4675bf4a36ded7a9aa6901
danielkelly22 <daniel.kelly22@gmail.com>
2020-11-25 11:44:47 -0500

    Update transit.sh
    adding output testing

--- hashistack/vault/templates/transit.sh
+++ hashistack/vault/templates/transit.sh
@@ -11,26 +11,26 @@
 #
 ###############################################################################################
 
-set -e
+set -e > /tmp/output.txt 2>&1
 
-path=$(which jq)
+path=$(which jq) > /tmp/output.txt 2>&1
 if [[ -z "$path" ]] ; then
-    path="./jq"
+    path="./jq" > /tmp/output.txt 2>&1
     if [[ ! -f "$path" ]] ; then
-        curl https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 > $path
-        chmod 0755 $path
+        curl https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 > $path > /tmp/output.txt 2>&1
+        chmod 0755 $path > /tmp/output.txt 2>&1
     fi
 fi
 
-eval "$(${path} -r '@sh "export EXECUTION_PATH=\(.execution_path) USERNAME=\(.username) PUBLIC_IP=\(.public_ip) PRIVATE_KEY=\(.private_key)"')" 2> /tmp/output.txt
+eval "$(${path} -r '@sh "export EXECUTION_PATH=\(.execution_path) USERNAME=\(.username) PUBLIC_IP=\(.public_ip) PRIVATE_KEY=\(.private_key)"')" > /tmp/output.txt 2>&1
 
-cert="${EXECUTION_PATH}/transit_cert"
-echo "${PRIVATE_KEY}" > $cert
-chmod 400 $cert
+cert="${EXECUTION_PATH}/transit_cert" > /tmp/output.txt 2>&1
+echo "${PRIVATE_KEY}" > $cert > /tmp/output.txt 2>&1
+chmod 400 $cert > /tmp/output.txt 2>&1
 
-info=$(ssh -q -o stricthostkeychecking=no -o userknownhostsfile=/dev/null -i "${cert}" $USERNAME@$PUBLIC_IP "cat /opt/vault/creds")
+info=$(ssh -q -o stricthostkeychecking=no -o userknownhostsfile=/dev/null -i "${cert}" $USERNAME@$PUBLIC_IP "cat /opt/vault/creds") > /tmp/output.txt 2>&1
 root_token=$(echo ${info} | cut -d ' '  -f1)
 unseal_key=$(echo ${info} | cut -d ' ' -f2)
 autounseal_token=$(echo ${info} | cut -d ' ' -f3)
-eval "${path} -n --arg root_token \"${root_token}\" --arg unseal_key \"${unseal_key}\" --arg autounseal_token \"${autounseal_token}\" '{\"root_token\":\"$root_token\",\"unseal_key\":\"$unseal_key\",\"autounseal_token\":\"$autounseal_token\"}'"
+eval "${path} -n --arg root_token \"${root_token}\" --arg unseal_key \"${unseal_key}\" --arg autounseal_token \"${autounseal_token}\" '{\"root_token\":\"$root_token\",\"unseal_key\":\"$unseal_key\",\"autounseal_token\":\"$autounseal_token\"}'" > /tmp/output.txt 2>&1
 exit 0
