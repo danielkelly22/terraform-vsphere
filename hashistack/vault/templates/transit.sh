354237854899ef1ed2296106263ca1bd638f983c
danielkelly22 <daniel.kelly22@gmail.com>
2020-11-25 13:20:42 -0500

    Update transit.sh
    remove logging

--- hashistack/vault/templates/transit.sh
+++ hashistack/vault/templates/transit.sh
@@ -11,7 +11,7 @@
 #
 ###############################################################################################
 
-set -e 1>&2
+set -e
 
 path=$(which jq)
 if [[ -z "$path" ]] ; then
@@ -22,15 +22,15 @@ if [[ -z "$path" ]] ; then
    fi
 fi
 
-eval "$(${path} -r '@sh "export EXECUTION_PATH=\(.execution_path) USERNAME=\(.username) PUBLIC_IP=\(.public_ip) PRIVATE_KEY=\(.private_key)"')" 1>&2
+eval "$(${path} -r '@sh "export EXECUTION_PATH=\(.execution_path) USERNAME=\(.username) PUBLIC_IP=\(.public_ip) PRIVATE_KEY=\(.private_key)"')"
 
-cert="${EXECUTION_PATH}/transit_cert"  2>&1
-echo "${PRIVATE_KEY}" > $cert 2>&1
-chmod 400 $cert  2>&1
+cert="${EXECUTION_PATH}/transit_cert"
+echo "${PRIVATE_KEY}" > $cert
+chmod 400 $cert
 
-info=$(ssh -q -o stricthostkeychecking=no -o userknownhostsfile=/dev/null -i "${cert}" $USERNAME@$PUBLIC_IP "cat /opt/vault/creds") 2>&1
+info=$(ssh -q -o stricthostkeychecking=no -o userknownhostsfile=/dev/null -i "${cert}" $USERNAME@$PUBLIC_IP "cat /opt/vault/creds")
 root_token=$(echo ${info} | cut -d ' '  -f1)
 unseal_key=$(echo ${info} | cut -d ' ' -f2)
 autounseal_token=$(echo ${info} | cut -d ' ' -f3)
-eval "${path} -n --arg root_token \"${root_token}\" --arg unseal_key \"${unseal_key}\" --arg autounseal_token \"${autounseal_token}\" '{\"root_token\":\"$root_token\",\"unseal_key\":\"$unseal_key\",\"autounseal_token\":\"$autounseal_token\"}'"  2>&1
+eval "${path} -n --arg root_token \"${root_token}\" --arg unseal_key \"${unseal_key}\" --arg autounseal_token \"${autounseal_token}\" '{\"root_token\":\"$root_token\",\"unseal_key\":\"$unseal_key\",\"autounseal_token\":\"$autounseal_token\"}'"
 exit 0
