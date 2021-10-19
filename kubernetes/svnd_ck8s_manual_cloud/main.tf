8f2db97fd54fee26427989470448bb2ba2dffdd2
John Puskar <john.puskar@amtrustgroup.com>
2021-10-19 11:36:08 -0400

    started on svnd-ck8s-manual-cloud

---
+++ kubernetes/svnd_ck8s_manual_cloud/main.tf
@@ -0,0 +1,130 @@
+terraform {
+//  required_version = ">= 0.14.5"
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+      version = "2.0.2"
+    }
+  }
+
+  backend "remote" {
+    hostname     = "tfe.amtrustgroup.com"
+    organization = "AmTrust-vSphere"
+
+    workspaces {
+      name = "terraform-vsphere--kubernetes-svndk8s06"
+    }
+  }
+}
+provider "vsphere" {
+  vim_keep_alive = 240
+  user           = var.vsphere_user
+  password       = var.vsphere_user_password
+  vsphere_server = var.vsphere_server
+  persist_session = true
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "datacenter" {
+  name = var.vsphere_datacenter_name
+}
+
+data "vsphere_datastore" "cluster" {
+  name          = var.vsphere_datastore_cluster_name
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data  "vsphere_compute_cluster" "cluster" {
+  name            = var.vsphere_compute_cluster_name
+  datacenter_id   = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vsphere_network_name
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_virtual_machine" "vm_template" {
+  name             = var.vsphere_template_name
+  datacenter_id    = data.vsphere_datacenter.datacenter.id
+}
+
+###
+# 1 easyrsa; 8GB
+# 3 etcd; 8GB
+# 1 kubeapi-load-balancer; mem=4G root-disk=8G
+# 2 kubernetes-master; cores=2 mem=4G root-disk=16G
+# 3 kubernetes-worker; cores=4 mem=4G root-disk=16G
+###
+
+resource "vsphere_virtual_machine" "vm" {
+  wait_for_guest_net_timeout = "30"
+  count                    = var.number_of_nodes
+  name                     = "${var.vm_name_prefix}${format("%s", element(var.alphabet_list, count.index))}"
+  resource_pool_id         = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id             = data.vsphere_datastore.cluster.id
+  folder                   = var.vsphere_vm_folder_name
+//  annotation		       = var.vm_annotation_1
+  guest_id                 = data.vsphere_virtual_machine.vm_template.guest_id
+  scsi_type                = data.vsphere_virtual_machine.vm_template.scsi_type
+  num_cpus                 = 4
+  memory                   = 16384
+  network_interface {
+    network_id    = data.vsphere_network.network.id
+  }
+
+  enable_disk_uuid = true
+  disk {
+    unit_number      = 0
+    label            = "${var.vm_name_prefix}${format("%s", element(var.alphabet_list, count.index))}_disk0.vmdk"
+    size             = data.vsphere_virtual_machine.vm_template.disks.0.size
+    thin_provisioned = data.vsphere_virtual_machine.vm_template.disks.0.thin_provisioned
+  }
+
+  disk {
+    unit_number      = 1
+    label            = "${var.vm_name_prefix}${format("%s", element(var.alphabet_list, count.index))}_disk1.vmdk"
+    size             = 30
+    thin_provisioned = true
+  }
+
+  clone {
+      template_uuid = data.vsphere_virtual_machine.vm_template.id
+      timeout = "900"
+      customize {
+        linux_options {
+          host_name = "${var.vm_name_prefix}${format("%s", element(var.alphabet_list, count.index))}"
+          domain = "amtrustservices.com"
+        }
+        network_interface {
+            ipv4_address = element(var.vm_netplan_ip_address_list, count.index)
+            ipv4_netmask = "24"
+        }
+        ipv4_gateway = var.vm_netplan_gateway
+        dns_server_list = var.vm_netplan_nameservers
+        dns_suffix_list = ["amtrustservices.com", "serv.infr.it.amtrustna.com"]
+        timeout = "120"
+      }
+  }
+
+#Install PIP, WinRM and Ansible
+//  provisioner "remote-exec" {
+//    inline = [
+//      "yum install python3 -y",
+//      "python3 -m pip install --upgrade --force-reinstall pip",
+//      "pip3 install pyvmomi",
+//      "python3 -m pip install --upgrade pip",
+//      "pip3 install ansible",
+//      "ansible --version",
+//    ]
+//    connection {
+//      host     = self.default_ip_address
+//      type     = "ssh"
+//      user     = var.centos_root_user
+//      password = var.centos_root_password
+//    }
+//  }
+
+}
---
+++ kubernetes/svnd_ck8s_manual_cloud/vars.tf
@@ -0,0 +1,104 @@
+###
+variable "vm_name_prefix" {
+  type=string
+  default="svndk8s06"
+}
+variable "vsphere_template_name" {
+  type=string
+  default="svn-ubuntu-2004-base-2021-07-06T174503"
+}
+variable "number_of_nodes" {
+  type=string
+  default=5
+}
+
+###
+variable "vsphere_user" {}
+variable "vsphere_server" {
+  type=string
+  default="clepvcsa01.amtrustservices.com"
+}
+
+variable "vsphere_datacenter_name" {
+  type=string
+  default="Seven Hills, OH"
+}
+
+variable "vsphere_vm_folder_name" {
+  type=string
+  default="kubernetes"
+}
+
+variable "vsphere_compute_cluster_name" {
+  type=string
+  default="AFSI-SVN"
+}
+
+variable "vsphere_datastore_cluster_name" {
+  type=string
+  default="svn_afsi_01"
+}
+
+variable "vsphere_network_name" {
+  type=string
+  default="VLAN 210 Development VDS"
+}
+
+variable "vm_netplan_gateway" {
+  type=string
+  default="10.59.210.1"
+}
+
+variable "vm_netplan_nameservers" {
+  type=list(string)
+  default=["10.59.10.21", "10.59.10.22"]
+}
+
+variable "vm_netplan_ip_address_list" {
+  type=list(string)
+  default=[
+    "10.59.210.73",
+    "10.59.210.74",
+    "10.59.210.75",
+    "10.59.210.76",
+    "10.59.210.77",
+  ]
+}
+
+//variable "vm_app_dev_ip_address" {}
+//variable "domain_name" {}
+
+###
+
+variable alphabet_list {
+  type = list(string)
+  default = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e"
+  ]
+}
+
+variable "vsphere_user_password" {type  = string}
+//variable "vm_root_password"      {type  = string}
+//variable "vm_root_user"          {type = string, default = "ubuntu"}
+//variable "virtual_machine_dns_servers" {
+//   type    = list(string)
+//}
+
+//variable "vm_name_linux_test" {
+//  description = "Name for the VM(s)"
+//  default = "clednixtftest"
+//}
+
+//variable "vm_annotation_1" {
+//  description = "Annotation to add to the VM(s)"
+//  default =  "Contact: Stephen Zuk \n Description: Terraform Test Server \n Ticket: N/A \n Created By: built via Terraform \n Created On: 07/31/2020"
+//}
+
+//variable "vm_count_dev_linux_test" {
+//  description = "Number of VMs to build"
+//  default = "1"
+//}
